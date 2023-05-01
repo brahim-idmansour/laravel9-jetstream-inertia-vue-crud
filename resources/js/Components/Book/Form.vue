8da77d68f6613db05260f93711bf9352feb6733e
brahim idmansour <brahimidmansour24@gmail.com>
2023-05-01 19:55:18 +0100

    fix form attibutes

--- resources/js/Components/Book/Form.vue
+++ resources/js/Components/Book/Form.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class="fixed z-10 inset-0 overflow-y-auto ease-out duration-400">
+    <div
+        class="fixed z-10 inset-0 overflow-y-auto ease-out duration-400"
+        v-if="isOpen"
+    >
         <div
             class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
         >
@@ -27,6 +30,7 @@
                                     >Title:
                                 </label>
                                 <input
+                                    v-model="form.title"
                                     type="text"
                                     class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                     id="formBookTitle"
@@ -40,6 +44,7 @@
                                     >Author:</label
                                 >
                                 <input
+                                    v-model="form.author"
                                     type="text"
                                     class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                     id="formBookAuthor"
@@ -69,6 +74,8 @@
                             class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto"
                         >
                             <button
+                                v-show="!isEdit"
+                                @click="$emit('formsave', form)"
                                 type="button"
                                 class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-green-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5"
                             >
@@ -79,6 +86,8 @@
                             class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto"
                         >
                             <button
+                                v-show="isEdit"
+                                @click="$emit('formsave', form)"
                                 type="button"
                                 class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-green-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5"
                             >
@@ -89,6 +98,7 @@
                             class="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto"
                         >
                             <button
+                                @click="$emit('formclose')"
                                 type="button"
                                 class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5"
                             >
